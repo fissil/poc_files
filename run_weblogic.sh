74245ce2047b478f61c6dffcbcb16094511dce70
Nikolay Zaytsev <nzaitsevc@gmail.com>
2020-02-24 11:32:03 +0200

    run exec in the background

--- run_weblogic.sh
+++ run_weblogic.sh
@@ -2,4 +2,4 @@
 docker run -ti -p 7011:7011 12213-medrec
 #http://localhost:7011/medrec
 weblogic_image = docker images 12213-medrec --format "{{.ID}}"
-docker exec -it $weblogic_image bash
+docker exec -dt $weblogic_image bash
