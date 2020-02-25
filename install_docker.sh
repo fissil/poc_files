d2e1dd6e290bad0e2bcbfd521730a14cd37337a1
Nikolay Zaytsev <nzaitsevc@gmail.com>
2020-02-25 16:54:49 +0200

    change the script for CC

--- install_docker.sh
+++ install_docker.sh
@@ -1,52 +1,53 @@
 #!/usr/bin/env bash
-yum install wget -y
-yum install curl -y
-curl -fsSL https://get.docker.com/ | sh
+sudo yum install wget -y
+sudo yum install curl -y
+sudo yum install git -y
+sudo curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
 sudo systemctl enable docker
-cd /home/centos
-git clone https://github.com/oracle/docker-images.git
+cd /home/cliqruser
+sudo git clone https://github.com/oracle/docker-images.git
 
 # Download WebLogic Docker install image
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1GSupO95dOjEDQxrO2zA0EuNEAtMDNdzA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1GSupO95dOjEDQxrO2zA0EuNEAtMDNdzA" -O fmw_12.2.1.3.0_wls_Disk1_1of1.zip && rm -rf /tmp/cookies.txt
+sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1GSupO95dOjEDQxrO2zA0EuNEAtMDNdzA' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1GSupO95dOjEDQxrO2zA0EuNEAtMDNdzA" -O fmw_12.2.1.3.0_wls_Disk1_1of1.zip && rm -rf /tmp/cookies.txt
 
 # Download Server JRE
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1tX6FRFw2S6MF_v-kRurex_qbmGjEN7Xx' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1tX6FRFw2S6MF_v-kRurex_qbmGjEN7Xx" -O server-jre-8u241-linux-x64.tar.gz && rm -rf /tmp/cookies.txt
+sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1tX6FRFw2S6MF_v-kRurex_qbmGjEN7Xx' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1tX6FRFw2S6MF_v-kRurex_qbmGjEN7Xx" -O server-jre-8u241-linux-x64.tar.gz && rm -rf /tmp/cookies.txt
 
 # Pull oracle image Linux
 sudo docker image pull oraclelinux:7-slim
 
 # Build Oracle JDK11
-#cd /home/centos/docker-images/OracleJava/java-11
+#cd /home/cliqruser/docker-images/OracleJava/java-11
 #docker build -t oracle/jdk:11 .
 
 # Build Oracle Server JRE8
-#mv fmw_12.2.1.3.0_wls_Disk1_1of1.zip /home/centos/docker-images/OracleJava/java-8
-mv server-jre-8u241-linux-x64.tar.gz /home/centos/docker-images/OracleJava/java-8
-cd /home/centos/docker-images/OracleJava/java-8
+#mv fmw_12.2.1.3.0_wls_Disk1_1of1.zip /home/cliqruser/docker-images/OracleJava/java-8
+sudo mv server-jre-8u241-linux-x64.tar.gz /home/cliqruser/docker-images/OracleJava/java-8
+sudo cd /home/cliqruser/docker-images/OracleJava/java-8
 sudo sh build.sh
 
 # Build Weblogic Server base image
-cd /home/centos/docker-images/OracleWebLogic/dockerfiles
-mv /home/centos/fmw_12.2.1.3.0_wls_Disk1_1of1.zip /home/centos/docker-images/OracleWebLogic/dockerfiles/12.2.1.3/
-sh buildDockerImage.sh -v 12.2.1.3 -g
+sudo cd /home/cliqruser/docker-images/OracleWebLogic/dockerfiles
+sudo mv /home/cliqruser/fmw_12.2.1.3.0_wls_Disk1_1of1.zip /home/cliqruser/docker-images/OracleWebLogic/dockerfiles/12.2.1.3/
+sudo sh buildDockerImage.sh -v 12.2.1.3 -g
 
 # Build the WebLogic Server 12cR2 with MedRec sample domain
-cd /home/centos/docker-images/OracleWebLogic/samples
-cp -r 12212-medrec 12213-medrec
-cd 12213-medrec
+sudo cd /home/cliqruser/docker-images/OracleWebLogic/samples
+sudo cp -r 12212-medrec 12213-medrec
+sudo cd 12213-medrec
 #Download WebLogic Server 12cR2 with MedRec sample domain
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QiEUE-aIVs9hnHAwvAp7qZERtDjQDq9Q' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QiEUE-aIVs9hnHAwvAp7qZERtDjQDq9Q" -O fmw_12.2.1.3.0_wls_supplemental_quick_Disk1_1of1.zip && rm -rf /tmp/cookies.txt
-sed -i 's/weblogic:12.2.1.2-developer/weblogic:12.2.1.3-generic/g' Dockerfile
-sed -i 's/fmw_12.2.1.2.0_wls_supplemental_quick_Disk1_1of1.zip/fmw_12.2.1.3.0_wls_supplemental_quick_Disk1_1of1.zip/g' Dockerfile
-sed -i 's/fmw_12.2.1.2.0_wls_supplemental_quick.jar/fmw_12.2.1.3.0_wls_supplemental_quick.jar/g' Dockerfile
+sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1QiEUE-aIVs9hnHAwvAp7qZERtDjQDq9Q' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1QiEUE-aIVs9hnHAwvAp7qZERtDjQDq9Q" -O fmw_12.2.1.3.0_wls_supplemental_quick_Disk1_1of1.zip && rm -rf /tmp/cookies.txt
+sudo sed -i 's/weblogic:12.2.1.2-developer/weblogic:12.2.1.3-generic/g' Dockerfile
+sudo sed -i 's/fmw_12.2.1.2.0_wls_supplemental_quick_Disk1_1of1.zip/fmw_12.2.1.3.0_wls_supplemental_quick_Disk1_1of1.zip/g' Dockerfile
+sudo sed -i 's/fmw_12.2.1.2.0_wls_supplemental_quick.jar/fmw_12.2.1.3.0_wls_supplemental_quick.jar/g' Dockerfile
 # creating SWAP of 2GB to satisfy the requirements
-dd if=/dev/zero of=/swapfile bs=1024 count=2048k
-mkswap /swapfile
-swapon /swapfile
+sudo dd if=/dev/zero of=/swapfile bs=1024 count=2048k
+sudo mkswap /swapfile
+sudo swapon /swapfile
 
 # build medrec application
-docker build -t 12213-medrec .
+sudo docker build -t 12213-medrec .
 
 
 # run the application if needed
