14304caf655a156e59bcc6384a614e4b02a84700
Nikolay Zaytsev <nzaitsevc@gmail.com>
2020-02-26 20:46:24 +0200

    Add sudo to run

--- run_weblogic.sh
+++ run_weblogic.sh
@@ -1,5 +1,5 @@
 # run the application if needed
-docker run -ti -p 7011:7011 12213-medrec
+sudo docker run -ti -p 7011:7011 12213-medrec
 #http://localhost:7011/medrec
 weblogic_image = docker images 12213-medrec --format "{{.ID}}"
-docker exec -dt $weblogic_image bash
+sudo ocker exec -dt $weblogic_image bash
